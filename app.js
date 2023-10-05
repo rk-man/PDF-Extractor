3a90908791901a11499ca62b91528c392a9c31d1
rk_man <naveenbadi2003@gmail.com>
2023-10-05 16:19:01 +0530

    Finished the pdf version of the assignment

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const uuid = require("uuid");
 const multer = require("multer");
+const { OpenAI } = require("openai");
 const nlp = require("compromise");
 const pdf = require("pdf-parse");
 const Word2Vec = require("word2vec");
@@ -7,6 +9,14 @@ const use = require("@tensorflow-models/universal-sentence-encoder");
 require("@tensorflow/tfjs-core");
 require("@tensorflow/tfjs-backend-cpu");
 require("@tensorflow/tfjs-node");
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({
+    node: "http://localhost:9200",
+});
+
+const openai = new OpenAI({
+    apiKey: process.env.CHATGPT_KEY,
+});
 
 const app = express();
 
@@ -18,6 +28,7 @@ const upload = multer();
 app.post("/upload", upload.single("file"), async (req, res) => {
     try {
         const file = req.file;
+        console.log(file);
         if (!file) {
             return res.status(400).json({ error: "No file uploaded" });
         }
@@ -47,14 +58,151 @@ app.post("/upload", upload.single("file"), async (req, res) => {
         const model = await use.load();
 
         const embeddings = await model.embed(chunks);
-        embeddings.print(true);
-        console.log(embeddings);
+        const vectors = embeddings.arraySync();
+        // embeddings.print(true);
+        console.log("CHUNKS");
+        console.log(chunks.length, chunks[0].length);
+        console.log("EMBEDDINGS");
+        console.log(vectors.length, vectors[0].length);
+
+        // CREATE AN INDEX
+        const indexName = "vector-embeddings";
+
+        if (!(await client.indices.exists({ index: indexName }))) {
+            await client.indices.create({
+                index: indexName,
+                mappings: {
+                    properties: {
+                        vectors: {
+                            type: "dense_vector",
+                            dims: 512,
+                            index: true,
+                            similarity: "l2_norm",
+                        },
+                        text: {
+                            type: "text",
+                        },
+                        filename: {
+                            type: "text",
+                        },
+                        doc_id: {
+                            type: "text",
+                        },
+                    },
+                },
+            });
+        }
+
+        const doc_id = uuid.v4();
+
+        const filename = file.originalname.split(".")[0];
+
+        const documents = [];
+
+        for (let idx = 0; idx < chunks.length; idx++) {
+            documents.push(
+                { index: { _index: indexName } },
+                {
+                    vectors: vectors[idx],
+                    text: chunks[idx],
+                    filename: filename,
+                    doc_id,
+                }
+            );
+        }
+
+        const bulk_ingestion = await client.bulk({
+            operations: documents,
+        });
+        console.log(bulk_ingestion.items);
 
         return res.status(200).json({
-            embeddings,
+            status: "success",
         });
     } catch (error) {
         console.error("Upload and Processing Error:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+app.get("/documents/:doc_id", async (req, res) => {
+    try {
+        const { query } = req.query;
+        const { doc_id } = req.params;
+        const indexName = "vector-embeddings";
+
+        // conver user's query to vector
+        const model = await use.load();
+
+        const embeddings = await model.embed(query);
+        const vectors = embeddings.arraySync();
+
+        // Define the user's query
+
+        // if (!query) {
+        //     return res.status(400).json({
+        //         message: "Please enter some query",
+        //     });
+        // }
+
+        const searchResponse = await client.search({
+            index: indexName,
+            body: {
+                knn: {
+                    field: "vectors",
+                    query_vector: vectors[0],
+                    k: 10, // number of most similar results
+                    num_candidates: 100,
+                },
+
+                query: {
+                    bool: {
+                        filter: [
+                            {
+                                term: {
+                                    doc_id,
+                                },
+                            },
+                        ],
+                    },
+                },
+            },
+        });
+
+        const searchResults = searchResponse.hits.hits.map((hit) => {
+            return hit._source.text;
+        });
+
+        const gpt_formatted_search_results = searchResults.map((sr) => {
+            return {
+                role: "user",
+                content: sr,
+            };
+        });
+
+        // open ai chat completion
+        const chatgpt_res = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        "You are someone who explains things in a simple and clear manner with the help of some sentences",
+                },
+                ...gpt_formatted_search_results,
+                {
+                    role: "user",
+                    content: query,
+                },
+            ],
+        });
+
+        return res.status(200).json({
+            status: "success",
+            results: chatgpt_res.choices[0].message.content,
+        });
+    } catch (err) {
+        console.log(err);
         return res.status(500).json({ error: "Internal server error" });
     }
 });
